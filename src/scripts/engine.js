bb959258edfd511f1d6e05388ac30da32f2148f9
ManuelaLuanaSchumackerTavares <manuelaluana.dev@gmail.com>
2024-01-13 18:20:51 -0300

    Adiciona cards de forma dinamica

---
+++ src/scripts/engine.js
@@ -0,0 +1,28 @@
+const emojis = [
+  "🤦‍♀️",
+  "🤦‍♀️",
+  "😎",
+  "😎",
+  "👀",
+  "👀",
+  "🎁",
+  "🎁",
+  "🤡",
+  "🤡",
+  "👹",
+  "👹",
+  "😺",
+  "😺",
+  "🚒",
+  "🚒",
+];
+let openCards = [];
+
+let shuffleEmojis = emojis.sort(() => (Math.random() > 0.5 ? 2 : -1));
+
+for (let i = 0; i < emojis.length; i++) {
+  let box = document.createElement("div");
+  box.className = "item";
+  box.innerHTML = shuffleEmojis[i];
+  document.querySelector(".game").appendChild(box);
+}
